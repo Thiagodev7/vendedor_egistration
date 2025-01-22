4600ec8ebb8b0f595e0cf940c6be0780f32eb4d3
Thiago Ribeiro <thiago.santos@uniodontogoiania.com.br>
2025-01-22 13:15:05 -0300

    dia 22/01

--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -1,6 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // Para copiar para a área de transferência
 import 'package:vendor_registration/modules/home/home_service.dart';
 import 'package:vendor_registration/modules/vendedor/vendedor_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomeController {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
@@ -64,21 +68,22 @@ class HomeController {
         email: email,
         cpf: cpfCnpj,
         linkVendas: link,
-        quantidadeVendas: 0,  // Defina a quantidade de vendas conforme necessário
+        quantidadeVendas: 0,
       );
 
       // Chamar o método do serviço para criar o vendedor
-      bool sucesso = await homeService.criarVendedor(vendedor);
+       bool sucesso = await homeService.criarVendedor(vendedor);
+       final qrCodeUrl = await homeService.generateQRCode(link);
 
-      if (sucesso) {
-        // Exibir mensagem de sucesso
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("Vendedor criado com sucesso!")),
-        );
+      if (sucesso && qrCodeUrl.isNotEmpty) {
+        // Exibir popup com o link gerado e o QR Code
+        _showLinkDialog(context, link, qrCodeUrl);
       } else {
         // Exibir mensagem de erro
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("Erro ao criar vendedor! Tente novamente mais tarde.")),
+          SnackBar(
+              content:
+                  Text("Erro ao criar vendedor! Tente novamente mais tarde.")),
         );
       }
 
@@ -88,10 +93,92 @@ class HomeController {
       // emailController.clear();
       // cpfCnpjController.clear();
     } else {
-      // Exibir mensagem de erro se o formulário não for válido
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Por favor, preencha todos os campos corretamente.")),
+        SnackBar(
+            content: Text("Por favor, preencha todos os campos corretamente.")),
       );
     }
   }
+
+  // Função para exibir o popup com o link e o QR Code
+  void _showLinkDialog(BuildContext context, String link, String qrCodeUrl) {
+    final String proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+final String proxiedUrl = qrCodeUrl;
+    print(qrCodeUrl);
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Link gerado com sucesso!"),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(link,
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 10),
+            if (qrCodeUrl.isNotEmpty)
+              // SizedBox(
+              //   width: 150,
+              //   height: 150,
+              //   child: CachedNetworkImage(
+              //     imageUrl: qrCodeUrl,
+              //     placeholder: (context, url) =>
+              //         const CircularProgressIndicator(),
+              //     errorWidget: (context, url, error) => const Icon(Icons.error),
+              //     httpHeaders: {
+              //       "Access-Control-Allow-Origin": "*"
+              //     }, // Adicione, se necessário
+              //   ),
+              // ),
+            Image.network(proxiedUrl),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Clipboard.setData(ClipboardData(text: link));
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                      content:
+                          Text("Link copiado para a área de transferência!")),
+                );
+              },
+              child: const Text("Copiar Link"),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(); // Fechar o popup
+            },
+            child: const Text("Fechar"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<Uint8List> fetchImage(String url) async {
+  final response = await http.get(Uri.parse(url));
+  if (response.statusCode == 200) {
+    return response.bodyBytes;
+  } else {
+    throw Exception('Erro ao carregar a imagem');
+  }
+}
+
+Widget buildImage(String url) {
+  return FutureBuilder<Uint8List>(
+    future: fetchImage(url),
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return const CircularProgressIndicator();
+      } else if (snapshot.hasError) {
+        return const Icon(Icons.error);
+      } else {
+        return Image.memory(snapshot.data!);
+      }
+    },
+  );
+}
+
 }
--- lib/modules/home/home_service.dart
+++ lib/modules/home/home_service.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:vendor_registration/modules/vendedor/vendedor_model.dart';
 import 'package:http/http.dart' as http;
 
 class HomeService {
-  static const String baseUrl = 'http://localhost:3000/vendedores'; // URL da sua API (use 10.0.2.2 para emulador)
+  static const String baseUrl =
+      'http://localhost:3000/vendedores'; // URL da sua API (use 10.0.2.2 para emulador)
 
   // Método para criar um novo vendedor
   Future<bool> criarVendedor(Vendedor vendedor) async {
@@ -38,6 +40,44 @@ class HomeService {
       }
 
       return false;
+    }
+  }
+
+  // Função para gerar o QR Code usando a API do QRplus
+  Future<String> generateQRCode(String link) async {
+    const String qrPlusApiUrl = "https://pro.qrplus.com.br/api/v1/qrcode";
+    const String apiKey =
+        "tLdZevkAF8G1huf8uAVyNHOV37qopq+nga78WbnbRI9jkfikEerlw03YjJwTMbcpPKTEzOYf7k9I/yOAJfD5kTe9KoQweMWNLFL8sLYu7Qg=";
+
+    try {
+      final response = await http.post(
+        Uri.parse(qrPlusApiUrl),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": apiKey,
+        },
+        body: json.encode({
+          "Title": "Vendedor Link", // Título do QR Code
+          "Dynamic": true, // Define se o QR Code será dinâmico ou estático
+          "Type": "url", // Tipo do QR Code (neste caso, um link)
+          "Content": "https://meusite.com/vendedor/dgdgdg"
+          //"Logo": null // URL do logo ou null se não houver logo
+        }),
+      );
+
+      if (response.statusCode == 200) {
+         final data = json.decode(response.body);
+      
+      return data["data"]["image"]; 
+      } else {
+        final responseData = json.decode(response.body);
+        final errorMessage = responseData['data'] ?? "Erro desconhecido";
+        print("Erro: $errorMessage");
+        return "";
+      }
+    } catch (e) {
+      print("Erro na requisição: $e");
+      return "";
     }
   }
 }
